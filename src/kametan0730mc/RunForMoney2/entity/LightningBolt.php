82d7e19bd261273198aba5a97cb51b205dd40d51
DaisukeDaisuke <akblcmakblcm@gmail.com>
2021-02-26 19:00:40 +0900

    fix bug #3(not tested)

--- src/kametan0730mc/RunForMoney2/entity/LightningBolt.php
+++ src/kametan0730mc/RunForMoney2/entity/LightningBolt.php
@@ -6,18 +6,17 @@ namespace kametan0730mc\RunForMoney2\entity;
 
 
 use pocketmine\entity\Entity;
+use pocketmine\entity\EntitySizeInfo;
 use pocketmine\entity\Living;
 use pocketmine\network\mcpe\protocol\types\entity\EntityIds;
 
 class LightningBolt extends Entity{
-	/* @var float */
-	public $width = 0.1;
-
-	/* @var float */
-	public $height = 0.1;
-
 	/** @var int */
 	public $age = 0;
+
+	protected function getInitialSizeInfo() : EntitySizeInfo{
+		return new EntitySizeInfo(0.1, 0.1);
+	}
 
 	public static function getNetworkTypeId(): string{
 		return EntityIds::LIGHTNING_BOLT;
@@ -30,4 +29,5 @@ class LightningBolt extends Entity{
 		}
 		return false;
 	}
+
 }
